18aab3a9988d11f28c04177ef5ff2d15f36b41b7
bysuses <rebusik67@gmail.com>
2023-05-19 17:45:37 +0200

    code formatted

--- MegaK/week6/day2/src/calculator.js
+++ MegaK/week6/day2/src/calculator.js
@@ -1,9 +1,9 @@
-const sum =(a,b) => a+b;
-const sub =(a,b) => a-b;
-const div =(a,b) => a/b;
-const mul =(a,b) => a*b;
+const sum = (a, b) => a + b;
+const sub = (a, b) => a - b;
+const div = (a, b) => a / b;
+const mul = (a, b) => a * b;
 
-module.exports={
+module.exports = {
     sum,
     sub,
     div,
